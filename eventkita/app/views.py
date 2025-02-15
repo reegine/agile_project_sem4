0d8ded53d4f573a63200b634dd1398458e7a8f93
Ivan Bunardi Junganda <23502310014@student.prasetiyamulya.ac.id>
2025-02-15 22:50:07 +0700

    login n footer

--- eventkita/app/views.py
+++ eventkita/app/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def forgotpassword_view(request):
+    return render(request, 'forgotpassword.html')
+
+def verify_view(request):
+    return render(request, 'verify.html')
+
+def reset_view(request):
+    return render(request, 'reset.html')
