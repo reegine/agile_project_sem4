d38cac32ea2add8410d317cdcb93ffb0daf8b588
reegine <regineangelina9@gmail.com>
2025-02-16 11:15:46 +0700

    add things

--- eventkita/app/urls.py
+++ eventkita/app/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('reset/', views.reset_view, name='reset'),
     path('aboutus/', views.about_us, name='about_us'), 
     path('detailpage/', views.detail_page, name='detail_page'), 
-    path('detailpagefree/', views.detail_page_free, name='detail_page'), 
-
+    path('detailpagefree/', views.detail_page_free, name='detail_page_free'), 
 
 ]
