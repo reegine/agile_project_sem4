af3c44dbba18aa78429e8cf6df8dc91dee9980ec
adikresna122 <adikresna122@gmail.com>
2025-02-20 21:11:18 +0700

    Login, Register, Logout Backend

--- eventkita/eventkita/settings.py
+++ eventkita/eventkita/settings.py
@@ -101,6 +101,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'app.CustomUser'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
